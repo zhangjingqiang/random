d25ef8e561d0d46a8c5e718ea35d53c4a3450c39
zhangjingqiang <zhangjingqiang@gmail.com>
2023-02-07 18:35:03 +0900

    add random feature

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+const webpack = require('webpack')
+require('dotenv').config()
+
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.plugins.push(new webpack.EnvironmentPlugin(process.env));
+    return config;
+  },
 }
 
 module.exports = nextConfig
